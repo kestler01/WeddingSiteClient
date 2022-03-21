e02df8c65726e4d61564cc342bac607d69b33913
kestler01 <kestler.andrew@gmail.com>
2022-03-21 12:24:34 -0400

    added production api url to config

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://amanda-andrew-wedding-api.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
